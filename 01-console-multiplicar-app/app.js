3689b490e039f1ac4202f28ba7e466084e8d16b4
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2019-12-30 23:58:24 -0500

    update: * new: separan las funciones en los archivos multiplicar y newFile * refactor app.js * new: readme.md

--- 01-console-multiplicar-app/app.js
+++ 01-console-multiplicar-app/app.js
@@ -5,24 +5,26 @@
  * Creados por nosotros ('./<foo>')
  */
 
-const argv = require('./config/yargs').argv;
-
-const { newFile, listarTabla } = require('./multiplicar/multiplicar');
+const argv = require("./config/yargs").argv;
+const { multiply, newFile } = require("./tools");
 
 let comando = argv._[0];
+console.log("TCL: comando", argv);
 
 switch (comando) {
-    case 'listar':
-        listarTabla(argv.base, argv.limit)
-        break;
+	case "listar":
+		multiply(argv.base, argv.limit).then(data => console.log(data));
+		break;
 
-    case 'crear':
-        newFile(argv.base, argv.limit)
-            .then(file => console.log(file))
-            .catch(err => console.error(err))
-        break;
+	case "crear":
+		multiply(argv.base).then(res =>
+			newFile(res)
+				.then(file => console.log(file))
+				.catch(err => console.error(err))
+		);
+		break;
 
-    default:
-        console.log('Comando no reconocido')
-        break;
+	default:
+		console.log("Comando no reconocido");
+		break;
 }
