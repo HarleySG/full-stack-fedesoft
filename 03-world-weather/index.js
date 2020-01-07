7bc9965f9632bbbdf4e065fbcb38abb4303c70fb
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-01-06 23:09:39 -0500

    update: refactor logia en comando argv.ciry para optener el clima de la ciudad requerida

--- 03-world-weather/index.js
+++ 03-world-weather/index.js
@@ -1,41 +1,24 @@
 const { argv } = require("./config");
-const { http } = require("./tools");
-
-global.$ = {
-	API: "https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php",
-	HOST: "",
-	KEYs: [
-		"x-rapidapi-key",
-		"4210f9688amsha5f4d40b9ec1809p11702ejsnb66bd378456f"
-	],
-	QUERYs: {
-		get: name => {
-			return {
-				method: "get",
-				baseURL: `${$.API}?location=${name}`,
-				headers: { [$.KEYs[0]]: $.KEYs[1] }
-			};
-		}
-	}
-};
+const { weatherService, locationService } = require("./service");
+const { weather } = require("./API");
+const { debuguear } = require("./tools");
 
 switch (argv._[0]) {
 	case "city":
 		console.log(argv.city);
-		const QUERY = $.QUERYs.get(argv.city);
-		http.queryBy(QUERY).then(res => {
-			try {
-				const { headers, status, data } = res;
-				if (status >= 200 && status < 300) {
-					const colombiaCities = data["Results"].filter(
-						city => city.c == "CO"
-					);
-					console.log(headers, status, colombiaCities);
+		locationService(argv.city)
+			.then(weatherService)
+			.then(request => {
+				if (request) {
+					const { weather, main, name, sys } = request.data;
+					const message = `El clima de ${name}, ${sys.country} es de ${main.temp}°C, ${weather[0].description}`;
+					console.log(message);
 				}
-			} catch (error) {
-				console.error(error);
-			}
-		});
+			})
+			.catch(e => {
+				console.error(e);
+			});
+
 		break;
 	default:
 		console.log(`Comando ${argv} no conocido`);
