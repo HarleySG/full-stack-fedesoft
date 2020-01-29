3e5106d63571d9af4343327cc99100584c669423
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-01-28 22:46:52 -0500

    delete: #submodule - migración repositorio udemy-nodejs-console-todo-list

--- 02-toDo-list/commands/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	del: require("./delete"),
-	add: require("./new"),
-	update: require("./update"),
-	list: require("./list")
-};
--- 02-toDo-list/commands/writeDB.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-module.exports = (file, data) => {
-	return fs.writeFile(file, JSON.stringify(data), err => {
-		if (err) throw err;
-		return { status: "success" };
-	});
-};
